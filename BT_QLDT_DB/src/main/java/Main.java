90dce9f38d7c147ed6184b08de664dd650635e83
thea <theasp1001@gmail.com>
2020-05-14 15:26:55 +0700

    Add slf4j logger

--- BT_QLDT_DB/src/main/java/Main.java
+++ BT_QLDT_DB/src/main/java/Main.java
@@ -1,6 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+	static Logger logger = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+		logger.debug("Debug");
 	}
 }
