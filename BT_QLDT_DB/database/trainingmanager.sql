1385ba171bf123ffe93971359cd9e201f44ebbdb
thea <theasp1001@gmail.com>
2020-05-14 16:07:03 +0700

    Update training manager sql file

--- BT_QLDT_DB/database/trainingmanager.sql
+++ BT_QLDT_DB/database/trainingmanager.sql
@@ -16,6 +16,7 @@ modified_by varchar(50) NULL
 CREATE TABLE Classroom( 
 id bigint NOT NULL PRIMARY KEY auto_increment,
 name varchar(50) NOT NULL,
+ministry_id bigint NOT NULL,
 created_at datetime NULL,
 modified_at datetime NULL,
 created_by varchar(50) NULL,
@@ -34,4 +35,14 @@ created_by varchar(50) NULL,
 modified_by varchar(50) NULL
 );
 
-ALTER TABLE Student ADD CONSTRAINT fk_classrom FOREIGN KEY (classroom_id) REFERENCES Classroom(id)
+ALTER TABLE Student ADD CONSTRAINT fk_student_classrom FOREIGN KEY (classroom_id) REFERENCES Classroom(id);
+ALTER TABLE Classroom ADD CONSTRAINT fk_classroom_ministry FOREIGN KEY (ministry_id) REFERENCES Ministry(id);
+
+INSERT INTO Ministry (first_name, last_name, email, password, phone, created_at, created_by)
+VALUES ("Nguyễn Thùy", "Chi", "nguyenthuychi@gmail.com", "Abcd1234", "0972145268", '2020-05-13 16:49:00', "Nguyễn Thùy Chi");
+
+INSERT INTO trainingmanager.Classroom (name, ministry_id, created_at) 
+VALUES ("12A1", 1, '2020-05-13 16:49:00'), 
+	("12A2", 1, '2020-05-13 16:49:00');
+    
+SELECT * FROM classroom;
